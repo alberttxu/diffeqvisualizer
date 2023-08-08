ce1bb8904a477eef2bf8a348da2e769a548d8858
Albert Xu <albert.t.xu@gmail.com>
2023-08-08 15:58:46 -0400

    first

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+CC=cc
+CFLAGS="-g3"
+WARNINGS="
+-Wall
+-Wextra
+-Wstrict-prototypes
+-Wdouble-promotion
+-Wno-unused-parameter
+-Wno-unused-function
+-Wconversion
+-Wno-sign-conversion
+"
+
+if [ $1 = "debug" ]; then
+   CFLAGS="$CFLAGS -O0"
+elif [ $1 = "sanitize" ]; then
+   CC="/opt/local/bin/clang-mp-16"
+   CFLAGS="$CFLAGS -fsanitize=address -fno-omit-frame-pointer -O3"
+elif [ $1 = "release" ]; then
+   CFLAGS="$CFLAGS -O3"
+else
+   echo "unrecognized build config: '$1'"
+   exit 1
+fi
+
+set -xe
+
+$CC $CFLAGS $WARNINGS -o main source_code/main.c
