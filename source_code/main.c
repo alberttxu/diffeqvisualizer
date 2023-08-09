f2740f8aba7b4c09f795aa863dae91fbcdc706bf
Albert Xu <albert.t.xu@gmail.com>
2023-08-09 19:06:31 -0400

    make ball move in a circle
    added function to convert math coordinates to pixel coords

--- source_code/main.c
+++ source_code/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
+
 #include <raylib.h>
 
 #include "useful_utils.c"
@@ -16,60 +18,63 @@ typedef int64_t i64;
 typedef float f32;
 typedef double f64;
 
+const int screenWidth = 800;
+const int screenHeight = 450;
+#define pixelsperunit 100
+
+// 0,0 = center of screen
+// +y = up
+// +x = right
+static inline
+Vector2 coords2pixels(Vector2 graph_coords)
+{
+   Vector2 pixel_coords = {
+       graph_coords.x * pixelsperunit + screenWidth / 2,
+      -graph_coords.y * pixelsperunit + screenHeight / 2
+   };
+   return pixel_coords;
+}
 
 int main(int argc, char **argv)
 {
-   const int screenWidth = 800;
-   const int screenHeight = 450;
-
    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
    SetTargetFPS(60);
-
-   Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
 
 #define histcapacity 20
    Vector2 recentBallPositions[histcapacity] = {0}; // ring buffer
    int curidx = 0;
    int histsize = 0;
 
-   f32 speed = 20;
+   f32 theta = 0;
+   f32 radius = 3;
 
    while (!WindowShouldClose())   // Detect window close button or ESC key
    {
-      if (IsKeyDown(KEY_RIGHT)) ballPosition.x += speed;
-      if (IsKeyDown(KEY_LEFT)) ballPosition.x -= speed;
-      if (IsKeyDown(KEY_UP)) ballPosition.y -= speed;
-      if (IsKeyDown(KEY_DOWN)) ballPosition.y += speed;
+      if (IsKeyDown(KEY_LEFT_SUPER) && IsKeyDown(KEY_W))
+         break;
 
-      if (ballPosition.x >= screenWidth)
-         ballPosition.x -= screenWidth;
-      if (ballPosition.x < 0)
-         ballPosition.x += screenWidth;
-      if (ballPosition.y >= screenHeight)
-         ballPosition.y -= screenHeight;
-      if (ballPosition.y < 0)
-         ballPosition.y += screenHeight;
-
-      BeginDrawing();
+      Vector2 ballPosition = {radius * cosf(theta), radius * sinf(theta)};
+      recentBallPositions[curidx] = coords2pixels(ballPosition);
       {
+         BeginDrawing();
          ClearBackground(RAYWHITE);
-         DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-
-         recentBallPositions[curidx] = ballPosition;
 
          for (int i = 0; i < histsize; i++)
          {
-            f32 radius = 10.0f - 0.5f * i;
+            f32 radius = 6.0f - 0.5f * i;
             int j = curidx - i;
             if (j < 0)
               j += histcapacity;
             DrawCircleV(recentBallPositions[j], radius, MAROON);
          }
-      }
-      EndDrawing();
 
+         DrawFPS(10, 10);
+         EndDrawing();
+      }
       curidx = (curidx+1) % histcapacity;
       histsize = min(histcapacity, histsize + 1);
+
+      theta += 0.05f;
    }
 
    CloseWindow();
