5303fcfbed5884c990eb756e2feba16396e453c4
Albert Xu <albert.t.xu@gmail.com>
2023-09-22 11:57:42 -0400

    use default value for INITIAL_MEMORY
    seems to work

--- build.sh
+++ build.sh
@@ -158,7 +158,7 @@ elif [ $1 = "tests" ]; then
 
 elif [ $1 = "web" ]; then
    CC=em++
-   CFLAGS="-D WEB -o index.html -s USE_GLFW=3 -s INITIAL_MEMORY=167772160"
+   CFLAGS="-D WEB -o index.html -s USE_GLFW=3"
    INCLUDES="\
    -I dependencies/raylib/src \
    -I libexec/emscripten/system/include \
