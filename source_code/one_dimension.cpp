0ca1c4d3f67a5cc9a58a5a582966e68e58a23d7e
Albert Xu <albert.t.xu@gmail.com>
2023-11-04 13:16:17 -0400

    change axis color for 1D demo

--- source_code/one_dimension.cpp
+++ source_code/one_dimension.cpp
@@ -4,11 +4,19 @@
 
 void gameloop_onedim()
 {
+   static f32 A = 0;
+   static f32 x_pos = 0;
+   Color axiscolor = BLACK;
+   if (A > 0)
+      axiscolor = GREEN;
+   else if (A < 0)
+      axiscolor = BLUE;
+
    // draw x axis
    {
       int x0 = screenwidth/2;
       int y0 = screenheight/2;
-      DrawLine(0, y0, screenwidth-1, y0, BLACK);
+      DrawLineEx((Vector2){0, (f32)y0}, (Vector2){(f32)screenwidth-1, (f32)y0}, 3, axiscolor);
       int ticklen = 5;
       for (int x = x0, xval = 0; x < screenwidth; x += pixelsperunit, xval += 1)
       {
@@ -28,9 +36,6 @@ void gameloop_onedim()
    DrawText(TextFormat("Frame time: %02.02f ms", drawtime_ms), 10, 50, 20, DARKGRAY);
    DrawText(TextFormat("t = %f", t), 10, 30, 20, DARKGRAY);
    DrawTexture(equation_texture, 250, 30, WHITE);
-
-   static f32 A = 0;
-   static f32 x_pos = 0;
 
    DrawCircleV(coords2pixels((Vector2){x_pos, 0}), 5, RED);
 
